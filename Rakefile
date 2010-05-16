78ddc7b5b21024eac270dde53be7dd8cc237c4f5
John OConnell <john.oconnell@gmail.com>
2010-05-16 14:46:17 -0400

    Refactored directory structure to organize projects. Moved space wolves to a project. Example project is now generic. TWR file is now generated in same folder as configuration yaml. Signed-off-by: John OConnell <john.oconnell@gmail.com>

--- Rakefile
+++ Rakefile
@@ -1,7 +1,6 @@
 require 'rake/clean'
 
 CLEAN.include %w( tmp/*.xml tmp/*.lua tmp )
-CLOBBER.include %w( twr/*.twr twr )
 
 desc "Generate xml and lua files. Usage: rake generate CONFIG="
 task :generate => :clean do
@@ -10,7 +9,7 @@ task :generate => :clean do
 end
 
 desc "Build twr file. Usage: rake build CONFIG="
-task :build => [:clobber, :generate] do
+task :build => [:generate] do
   raise "CONFIG= is required" unless ENV.include?("CONFIG")
   sh "ruby script/build.rb #{ENV["CONFIG"]}"
 end
--- script/build.rb
+++ script/build.rb
@@ -3,17 +3,16 @@ require 'yaml'
 include Archive::Tar
 
 # setup constants and create twr directory
-TWR_DIR  = File.expand_path(File.dirname(__FILE__) + "/../twr")
 TMP_DIR  = File.expand_path(File.dirname(__FILE__) + "/../tmp")
 CFG_FILE = File.expand_path(ARGV[0])
-Dir.mkdir(TWR_DIR)
+CFG_DIR  = File.dirname(CFG_FILE)
 Dir.chdir(TMP_DIR)
 
 # read config file
 cfg = YAML::load_file(CFG_FILE)
 
 # create twr file
-File.open(File.join(TWR_DIR, "#{cfg['ruleset'].gsub(/ /, '-')}-#{cfg['name'].gsub(/ /, '-')}.twr"), "wb") do |twr|
+File.open(File.join(CFG_DIR, "#{cfg['ruleset'].gsub(/ /, '-')}-#{cfg['name'].gsub(/ /, '-')}.twr"), "wb") do |twr|
   files = Dir.glob(TMP_DIR + "/*").map{|f| File.basename(f)}
   Minitar.pack(files, twr)
 end
